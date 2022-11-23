eacfd821e078bb859bda485127d74431035306d2
WilliamKoo <william.w.gu@outlook.com>
2022-11-23 23:26:02 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Remarkable } from 'remarkable';
+import { useState } from 'react';
 
 function App() {
+
+  const [value, setValue] = useState('Type some *markdown* here!');
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const getMarkdown = () => {
+    const md = new Remarkable();
+    return {__html: md.render(value)};
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='input'>
+        <h3>Input</h3>
+        <textarea className='input-text' onChange={handleChange} defaultValue={value} />
+      </div>
+      <div className='output'>
+        <h3>Markdown</h3>
+        <div dangerouslySetInnerHTML={getMarkdown()} className='output-text' />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
